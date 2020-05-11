16ee1b427b759afc5be712825bdc70f4b70e6900
Allan Corinaldi <allan9899@gmail.com>
2020-05-11 07:00:38 -0500

    deleting dotenv

--- modules/Passport.js
+++ modules/Passport.js
@@ -2,7 +2,6 @@ const passport = require("passport");
 const Strategy = require("passport-local").Strategy;
 const mongo = require("./MongoUtils");
 const crypto = require("crypto");
-require("dotenv").config();
 
 passport.use(
   new Strategy((username, password, cb) => {
