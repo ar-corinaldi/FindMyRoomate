91137cefeaff60be305614c9bbb75b067e73336f
Dartm05 <d.arteaga@uniandes.edu.co>
2020-05-27 19:53:04 -0500

    forms act

--- front/src/components/CarouselControlled.js
+++ front/src/components/CarouselControlled.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
+
 function CarouselControlled(props) {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
+
+ 
+
   return (
     <Carousel activeIndex={index} onSelect={handleSelect} className="w-100">
       {!props.rooms
-        ? "No rooms yet"
+        ? <h1>"No rooms yet"</h1>
         : props.rooms.map((room, index) => {
             return (
               <Carousel.Item key={index}>
@@ -21,6 +25,8 @@ function CarouselControlled(props) {
                   <h3>{props.address}</h3>
                   <p>{props.price}</p>
                 </Carousel.Caption>
+
+                <button className="btn" onClick={()=> handleSelect} >Not available</button>
               </Carousel.Item>
             );
           })}
